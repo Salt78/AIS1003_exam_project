72f746925a0afbc56775095f4fd662e96cf0a493
Andreas A. Vannebo <andreas.alexander@outlook.com>
2024-11-02 14:20:11 +0100

    gridManager.hpp Implemented functions for retrieving coords, logging and checking if a set of coords have been used.

--- include/gridManager.hpp
+++ include/gridManager.hpp
@@ -6,17 +6,32 @@
 #define GRIDMANAGER_HPP
 
 #include <map>
-#include <utility>
+#include <vector>
+#include <algorithm>
 
 class GridManager {
 private:
-    float m_gridSize{};
+    // The size of the image. Should always be a square.
     float m_imageSize{};
+    // The size of the grid. Always n X n
+    float m_gridSize{};
+    // The spacing between each assigned coordinate.
     int m_spacing{};
+    // A map to store the grid coordinates.
     std::map<int, std::pair<float, float> > m_gridMap{};
+    // A vector to store the used coordinates.
+    std::vector<int> m_usedCoords{};
+
+
+    //Googled to figure out what i could use in the standard library to sort a vector. https://www.digitalocean.com/community/tutorials/sorting-a-vector-in-c-plus-plus
+    // I originally landed on std::sort, but Clion recommended std::ranges::sort instead. The code is definitely cleaner.
+    void logUsedCoords(int key) {
+        m_usedCoords.push_back(key);
+        std::ranges::sort(m_usedCoords);
+    }
 
 public:
-    GridManager(float imageSize, float gridSize = 14, int spacing = 50)
+    explicit GridManager(float imageSize, float gridSize, int spacing)
         : m_imageSize(imageSize), m_gridSize(gridSize), m_spacing(spacing) {
     };
 
@@ -35,11 +50,23 @@ public:
                 m_gridMap[key] = std::make_pair(startingCoords.first + z * m_spacing,
                                                 startingCoords.second + i * m_spacing);
                 // DEBUG
-                int xCoord = startingCoords.first + z * m_spacing;
-                int yCoord = startingCoords.second + i * m_spacing;
+                //int xCoord = startingCoords.first + z * m_spacing;
+                //int yCoord = startingCoords.second + i * m_spacing;
                 key++;
             }
         }
+    }
+
+    // Got some help to fix const correctness from GPT (using .at())
+    std::pair<int, int> getCoords(const int key) {
+        logUsedCoords(key);
+        return m_gridMap.at(key);
+    }
+
+    //I originally was recommended to use std::find https://stackoverflow.com/questions/571394/how-to-find-out-if-an-item-is-present-in-a-stdvector
+    //But the second most popular answer recommended std::binary_search instead when dealing with larger vectors.
+    bool isUsed(int key) {
+        return std::binary_search(m_usedCoords.begin(), m_usedCoords.end(), key);
     }
 
     // auto getTestValue(int key) const {
