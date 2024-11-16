752216854a4fbbce9995ca9cddaa50aed433cbbf
Andreas A. Vannebo <andreas.alexander@outlook.com>
2024-11-16 18:55:09 +0100

    Made changes to workflows

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -9,23 +9,10 @@ using namespace gridManagerNS;
 using namespace geoDetectionNS;
 
 
-/*TEST_CASE("Scene creation", "[scene]") {
-    std::pair<int, int> imageSize{800, 800};
-    const int meshQuantity{5};
-
-    GridManager mainGrid("GeometrySorting", imageSize, 14, 50);
-    mainGrid.createGrid();
-
-    GeoGen test(40, meshQuantity, geoGenNS::Shape::CUBE, Color::aqua);
-    test.generate(mainGrid);
-
-    std::vector<Object3D *> presentMeshes = mainGrid.getScene()->children;
-
-    REQUIRE(presentMeshes.size() == meshQuantity);
-}*/
 
 
-TEST_CASE("Grid creation", "[grid]") {
+
+/*TEST_CASE("Grid creation", "[grid]") {
     std::pair<int, int> imageSize{800, 800};
 
     GridManager mainGrid("GeometrySorting", imageSize, 14, 50);
@@ -35,4 +22,20 @@ TEST_CASE("Grid creation", "[grid]") {
     REQUIRE(mainGrid.isUsed(1) == false);
     REQUIRE(mainGrid.getCoords(1) == std::pair<float, float>(50, 50));
     REQUIRE(mainGrid.isUsed(1) == true);
+}*/
+
+
+TEST_CASE("Scene creation", "[scene]") {
+    std::pair<int, int> imageSizee{800, 800};
+    const int meshQuantity{5};
+
+    GridManager grid2("Scene creation", imageSizee, 14, 50);
+   grid2.createGrid();
+
+    GeoGen test(40, meshQuantity, geoGenNS::Shape::CUBE, Color::aqua);
+    test.generate(grid2);
+
+    std::vector<Object3D *> presentMeshes = grid2.getScene()->children;
+
+    REQUIRE(presentMeshes.size() == meshQuantity);
 }
