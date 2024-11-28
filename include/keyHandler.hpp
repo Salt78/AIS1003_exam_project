2a6c50f481e649b7325f2ca15b3705cf1eaea817
Andreas A. Vannebo <andreas.alexander@outlook.com>
2024-11-29 00:24:03 +0100

    Optimized keyHandler

--- include/keyHandler.hpp
+++ include/keyHandler.hpp
@@ -6,65 +6,32 @@
 #include "geoManipulator.hpp"
 #include <threepp/threepp.hpp>
 
-using namespace threepp;
-using namespace geoDetectionNS;
-using namespace geoGenNS;
-using namespace geoManipulatorNS;
-
-
-class KeyHandler : public KeyListener {
+/**
+ * @brief Handles key events
+ */
+class KeyHandler final : public threepp::KeyListener {
 private:
-    GeoGen &m_generator;
-    GeoDetection &m_geoDetection;
-    GeoManipulator &m_geoManipulator;
+    geoGenNS::GeoGen &m_generator;
+    geoDetectionNS::GeoDetection &m_geoDetection;
+    geoManipulatorNS::GeoManipulator &m_geoManipulator;
 
     bool m_spaceBarPressed{false};
     bool m_rPressed{false};
     bool m_xPressed{false};
 
+  void onKeyPressed(threepp::KeyEvent evt) override;
+
+
+  void onKeyReleased(threepp::KeyEvent evt) override;
+
 
 public:
-    explicit KeyHandler(GeoGen &generator, GeoDetection &geoDetection, GeoManipulator &geoManipulator)
-        : m_generator(generator), m_geoDetection(geoDetection), m_geoManipulator(geoManipulator) {}
+    explicit KeyHandler(geoGenNS::GeoGen &generator, geoDetectionNS::GeoDetection &geoDetection, geoManipulatorNS::GeoManipulator &geoManipulator);
 
-    void onKeyPressed(KeyEvent evt) override {
-        if (evt.key == Key::SPACE) {
-            m_spaceBarPressed = true;
-        }
-        if (evt.key == Key::R) {
-            m_rPressed = true;
-        }
-        if (evt.key == Key::X) {
-            m_xPressed = true;
-        }
-    }
 
-    void onKeyReleased(KeyEvent evt) override {
-        if (evt.key == Key::SPACE) {
-            m_spaceBarPressed = false;
-        }
-        if (evt.key == Key::R) {
-            m_rPressed = false;
-        }
-        if (evt.key == Key::X) {
-            m_xPressed = false;
-        }
-    }
-
-    void update() {
-        if (m_spaceBarPressed) {
-            m_geoDetection.contourDetection();
-            m_geoManipulator.reArrangeMeshes(m_geoDetection.getDetectedObjects());
-        }
-        if (m_rPressed) {
-            m_geoDetection.cleanUp();
-            m_geoManipulator.cleanUp();
-
-            m_generator.generateRND();
-        }
-        if (m_xPressed) {
-            m_geoDetection.previewDetection();
-        }
-    }
+    /**
+     * @brief Checks for key presses and updates the scene accordingly.
+     */
+    void update() const;
 };
 #endif//KEYLISTENER_HPP
--- src/keyHandler.cpp
+++ src/keyHandler.cpp
@@ -1,1 +1,53 @@
 #include "keyHandler.hpp"
+
+using namespace threepp;
+using namespace geoGenNS;
+using namespace geoDetectionNS;
+using namespace geoManipulatorNS;
+
+
+void KeyHandler::onKeyPressed(const KeyEvent evt) {
+    if (evt.key == Key::SPACE) {
+        m_spaceBarPressed = true;
+    }
+    if (evt.key == Key::R) {
+        m_rPressed = true;
+    }
+    if (evt.key == Key::X) {
+        m_xPressed = true;
+    }
+}
+
+
+void KeyHandler::onKeyReleased(const KeyEvent evt) {
+    if (evt.key == Key::SPACE) {
+        m_spaceBarPressed = false;
+    }
+    if (evt.key == Key::R) {
+        m_rPressed = false;
+    }
+    if (evt.key == Key::X) {
+        m_xPressed = false;
+    }
+}
+
+
+KeyHandler::KeyHandler(GeoGen &generator, GeoDetection &geoDetection, GeoManipulator &geoManipulator)
+    : m_generator(generator), m_geoDetection(geoDetection), m_geoManipulator(geoManipulator) {}
+
+
+void KeyHandler::update() const {
+    if (m_spaceBarPressed) {
+        m_geoDetection.contourDetection();
+        m_geoManipulator.reArrangeMeshes(m_geoDetection.getDetectedObjects());
+    }
+    if (m_rPressed) {
+        m_geoDetection.cleanUp();
+        m_geoManipulator.cleanUp();
+
+        m_generator.generateRND();
+    }
+    if (m_xPressed) {
+        m_geoDetection.previewDetection();
+    }
+}
